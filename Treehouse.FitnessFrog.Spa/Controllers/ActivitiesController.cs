d52ab885b55b1b4e591f38f4d76cc23b23a7a7ef
bahmanu <bahman_mj_hp@yahoo.com>
2018-04-11 15:11:24 +0100

    Activities and intensities Contorllers Added - DTO Implemented

---
+++ Treehouse.FitnessFrog.Spa/Controllers/ActivitiesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Treehouse.FitnessFrog.Shared.Data;
+
+namespace Treehouse.FitnessFrog.Spa.Controllers
+{
+    public class ActivitiesController : ApiController
+    {
+        private ActivitiesRepository _activitiesRespository = null;
+
+        public ActivitiesController(ActivitiesRepository activitiesRepository)
+        {
+            _activitiesRespository = activitiesRepository;
+        }
+
+        public IHttpActionResult Get()
+        {
+            return Ok(_activitiesRespository.GetList());
+        }
+    }
+}
